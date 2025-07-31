355287e99eb26b6098de8d9a14285122d72d31a2
traher228 <147732271+baher228@users.noreply.github.com>
2025-07-31 15:58:06 +0300

    Add fabrik-clone/src/App.tsx 1753966686237

---
+++ fabrik-clone/src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Careers from './pages/Careers';
+import Places from './pages/Places';
+import NotFound from './pages/NotFound';
+import './styles/global.css';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/who-we-are" element={<About />} />
+            <Route path="/careers" element={<Careers />} />
+            <Route path="/places" element={<Places />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
